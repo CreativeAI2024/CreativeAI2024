afaf36a8376531bb7f2a2980d04a2f8ba44ef9b3
uwatcha <discoverywant30@gmail.com>
2024-05-29 17:00:40 +0900

    ウィンドウをインスタンス化時に非アクティブ化し、active/非activeに変更できるメソッドOpenWindow()とCloseWindow()を作成した。

--- Assets/Scripts/BaseWindow.cs
+++ Assets/Scripts/BaseWindow.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+// ウィンドウを開く、閉じる機能、コンストラクタで縦横比率等を指定できるようにする
+public class BaseWindow : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        this.CloseWindow();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OpenWindow() 
     {
-        
+        this.gameObject.SetActive(true);
+    }
+
+    void CloseWindow()
+    {
+        this.gameObject.SetActive(false);
     }
 }
