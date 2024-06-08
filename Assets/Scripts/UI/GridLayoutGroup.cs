f65d576a5b2b7b676a547ebc8358c42477cdb013
uwatcha <discoverywant30@gmail.com>
2024-06-08 22:15:23 +0900

    GridLayoutGroup.csをリファクタ

--- Assets/Scripts/UI/GridLayoutGroup.cs
+++ Assets/Scripts/UI/GridLayoutGroup.cs
@@ -6,34 +6,10 @@ using UnityEngine.UI;
 
 public class GridLayoutGroup : MonoBehaviour
 {
-    private List<Button> buttons;
     [SerializeField] Button headButton;
-    void Start()
-    {
-        buttons = new List<Button>();
-
-        foreach (Transform child in transform)
-        {
-            Button buttonTmp = child.GetComponent<Button>();
-            if (buttonTmp != null)
-            {
-                buttons.Add(buttonTmp);
-            }
-            else
-            {
-                Debug.Log("There are no buttons in " + gameObject);
-            }
-        }
-    }
-
-    void Update()
-    {
-
-    }
-
     void OnEnable()
     {
-        StartCoroutine("focusHeadButton");
+        focusHeadButton();
     }
 
     private void focusHeadButton()
