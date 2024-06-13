6d46cc3481e87a8880c1262e8a00fa667206a1c4
uwatcha <discoverywant30@gmail.com>
2024-06-13 21:19:42 +0900

    Revert "ウィンドウを閉じた時にフォーカスやアクティブをリセットする機能を実装中"
    This reverts commit 3e1674d71df9079ae681a738d08a661a51dd643b.
    
    resetWindowStateの作業は別ブランチで行うようにするため、feat/uiでの変更はrevertする

--- Assets/Scripts/UI/ToggleWindowAction.cs
+++ Assets/Scripts/UI/ToggleWindowAction.cs
@@ -16,13 +16,12 @@ public class ToggleWindowAction : MonoBehaviour
             ToggleWindow();
         }
     }
-    public void ToggleWindow()
+    public void ToggleWindow() //どのウィンドウを開けてるかの状態もリセットしたい。各ウィンドウのdisenable()の中で処理した方がいい？
     {
         ChangeActive(windowBox, !windowBox.activeSelf);
     }
     private void ChangeActive(GameObject gameObject, bool isActive)
     {
-        Debug.Log(gameObject);
         gameObject.SetActive(isActive);
     }
 }
