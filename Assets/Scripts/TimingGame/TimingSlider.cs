e56f21649bd7c3185a70c9b609e2d1922da6db9b
kanata0510 <54180109+kanata0510@users.noreply.github.com>
2024-06-29 21:36:45 +0900

    Update Assets/Scripts/TimingGame/TimingSlider.cs

--- Assets/Scripts/TimingGame/TimingSlider.cs
+++ Assets/Scripts/TimingGame/TimingSlider.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Slider))]
 public class TimingSlider: MonoBehaviour
 {
     private Slider timingSlider;
