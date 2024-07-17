68fa2d9b2ddba04168aaf08148c9bfa728e6b6db
5884.u <b1022235@fun.ac.jp>
2024-07-17 16:55:39 +0900

    rushGameFirst

---
+++ Assets/Scripts/RushGame/RushGameMain.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+
+public class RushGameMain : MonoBehaviour
+{
+    [SerializeField] private float goalCount;
+    private float rushedCount;
+    [SerializeField] private float limitTime;
+    private float ScheduleTime;
+    private InputSetting _inputSetting;
+
+    [SerializeField] private RushGameSlider rushGameSlider;
+
+    void Start()
+    {
+        Initialize();
+    }
+
+    void Update()
+    {
+        ScheduleTime += Time.deltaTime;
+        if (ScheduleTime < limitTime)
+        {
+            Proceed();
+            if (!(rushGameSlider == null))
+            {
+                UIControl();
+            }
+        }
+        else if(_inputSetting.GetDecideKeyDown())
+        {
+            if (rushedCount > goalCount)
+            {
+                Debug.Log("success");     //成功したとき
+            }
+            else
+            {
+                Debug.Log("fail");     //失敗したとき
+            }
+        }
+    }
+
+    public void Initialize()
+    {
+        _inputSetting = InputSetting.Load();
+        ScheduleTime = 0;
+    }
+    public void Proceed()
+    {
+        if (_inputSetting.GetDecideKeyDown())
+        {
+            rushedCount++;
+        }
+    }
+
+    public void UIControl()
+    {
+        rushGameSlider.SetSliderValue(rushedCount / goalCount);
+        rushGameSlider.SetGoalCountText(goalCount.ToString());
+        rushGameSlider.SetTimeLimit(ScheduleTime.ToString());
+        rushGameSlider.SetPressCountText(rushedCount.ToString());
+    }
+}
---
+++ Assets/Scripts/RushGame/RushGameSlider.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RushGameSlider : MonoBehaviour
+{
+    [SerializeField] private Slider RushgameSlider;
+
+    [SerializeField] private TextMeshProUGUI timeLimit;
+    [SerializeField] private TextMeshProUGUI pressCountText;
+    [SerializeField] private TextMeshProUGUI goalCountText;
+
+    private void Start()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        RushgameSlider = GetComponent<Slider>();
+        RushgameSlider.value = 0;
+    }
+
+    public void SetTimeLimit(string text)
+    {
+        if (timeLimit != null)
+        {
+            timeLimit.text = text;
+        }
+    }
+
+    public void SetPressCountText(string text)
+    {
+        if (pressCountText != null)
+        {
+            pressCountText.text = text;
+        }
+    }
+
+    public void SetGoalCountText(string text)
+    {
+        if (goalCountText != null)
+        {
+            goalCountText.text = text;
+        }
+    }
+
+    public void SetSliderValue(float value)
+    {
+        RushgameSlider.value = value;
+    }
+
+}
