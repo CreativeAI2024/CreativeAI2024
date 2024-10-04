f54aa1b9de534af06f5eaa56cd683c1a99a00199
kanata0510 <kawakana0510@gmail.com>
2024-10-04 15:40:49 +0900

    add FlagManager and Singleton base

--- Assets/Scripts/FlagManager.cs
+++ Assets/Scripts/FlagManager.cs
@@ -1,19 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
 
-public class FlagManager : MonoBehaviour
+public class FlagManager : DontDestroySingleton<FlagManager>
 {
     private int progressFlag = 0;
-    // Start is called before the first frame update
-    void Start()
+    private int experienceFlag = 0;
+    
+    public void AddProgressFlag(int number) => AddFlag(ref progressFlag, number);
+    public void DeleteProgressFlag(int number) => DeleteFlag(ref progressFlag, number);
+    public bool HasProgressFlag(int number) => HasFlag(ref progressFlag, number);
+    
+    public void AddExperienceFlag(int number) => AddFlag(ref experienceFlag, number);
+    public void DeleteExperienceFlag(int number) => DeleteFlag(ref experienceFlag, number);
+    public bool HasExperienceFlag(int number) => HasFlag(ref experienceFlag, number);
+    
+    private void AddFlag(ref int bitFlag, int number)
     {
-        
+        bitFlag |= 1 << number;
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private void DeleteFlag(ref int bitFlag, int number)
     {
-        
+        bitFlag &= ~(1 << number);
+    }
+    
+    private bool HasFlag(ref int bitFlag, int number)
+    {
+        return Convert.ToBoolean(bitFlag & (1 << number));
     }
 }
