fc38338e1996cb96a2d2743381941dfbc46d8a59
KOMATSU CHIKA <128227320+KOMATSU-CHIKA@users.noreply.github.com>
2024-10-24 00:47:15 +0900

    Fixed

--- Assets/Scripts/SaveUtility.cs
+++ Assets/Scripts/SaveUtility.cs
@@ -7,16 +7,10 @@ public static class SaveUtility
     public static T JsonToData<T>(string filePath) where T : class
     {
         //JSONファイルからデータ読み込み、指定した型にデシリアライズ
-        if (!File.Exists(filePath))
-        {
-            throw new FileNotFoundException("JSON file not found: " + filePath);
-        }
-
         string jsonContent = File.ReadAllText(filePath);
         byte[] msgPackData = MessagePackSerializer.ConvertFromJson(jsonContent);
-        T data = MessagePackSerializer.Deserialize<T>(msgPackData);
 
-        return data;
+        return MessagePackSerializer.Deserialize<T>(msgPackData);
     }
     
     public static void DataToJson<T>(T data, string savePath) where T : class
@@ -30,21 +24,13 @@ public static class SaveUtility
     public static T SaveFileToData<T>(string filePath) where T : class
     {
         //指定したファイルパスからデータを読み込み、デシリアライズ
-        if (!File.Exists(filePath))
-        {
-            throw new FileNotFoundException("Save file not found: " + filePath);
-        }
-
         byte[] msgPackData = File.ReadAllBytes(filePath);
-        T data = MessagePackSerializer.Deserialize<T>(msgPackData);
 
-        return data;
+        return MessagePackSerializer.Deserialize<T>(msgPackData);
     }
     
     public static void DataToSaveFile<T>(T data, string savePath) where T : class
     {
-        string jsonContent = JsonUtility.ToJson(data);
-        File.WriteAllText($"{savePath}", jsonContent);
         byte[] msgPackData = MessagePackSerializer.Serialize(data);
         File.WriteAllBytes(savePath, msgPackData);
     }
