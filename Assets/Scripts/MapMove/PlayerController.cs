dfc68da904bf6f9d73b7ba5ab690802454cefdf0
kanata0510 <54180109+kanata0510@users.noreply.github.com>
2024-10-27 17:09:02 +0900

    Update PlayerController.cs

--- Assets/Scripts/MapMove/PlayerController.cs
+++ Assets/Scripts/MapMove/PlayerController.cs
@@ -70,7 +70,7 @@ public class PlayerController : MonoBehaviour
 
     protected virtual void Move(Vector3 vector)
     {
-        _playerTransform.position += Time.deltaTime * 10000 * vector.normalized * speed;
+        _playerTransform.position += Time.deltaTime * 100 * vector.normalized * speed;
     }
 
     void MoveEnd()
