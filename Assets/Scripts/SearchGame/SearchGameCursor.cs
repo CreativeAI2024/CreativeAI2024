4a70c1b3bce24f17042e1a007bad653a90fd0cb7
uwatcha <discoverywant30@gmail.com>
2024-11-01 16:42:17 +0900

    カーソルの移動範囲限定できた

--- Assets/Scripts/SearchGame/SearchGameCursor.cs
+++ Assets/Scripts/SearchGame/SearchGameCursor.cs
@@ -1,39 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SearchGameCursor : MonoBehaviour
 {
-    [SerializeField] private Canvas canvas;
     private InputSetting _inputSetting;
-    public float speed = 300.0f;
+    [SerializeField] private RectTransform canvas;
+    [SerializeField] private RectTransform cursor;
+    [SerializeField] private RectTransform judgePoint;
+    private const float speed = 300.0f;
+    private float cursorTipOffsetX;
+    private float cursorTipOffsetY;
+
     void Start()
     {
         _inputSetting = InputSetting.Load();
+        cursorTipOffsetX = cursor.rect.height/2 - judgePoint.anchoredPosition.x;
+        cursorTipOffsetY = cursor.rect.width/2 - judgePoint.anchoredPosition.y;
     }
 
     void Update()
     {
-        if (_inputSetting.GetForwardKey())
+        if (_inputSetting.GetForwardKey() && !IsOnUpEdge())
         {
             transform.position += speed * Time.deltaTime * Vector3.up;
         }
-        if (_inputSetting.GetBackKey())
+        if (_inputSetting.GetBackKey() && !IsOnDownEdge())
         {
             transform.position += speed * Time.deltaTime * Vector3.down;
         }
-        if (_inputSetting.GetLeftKey())
+        if (_inputSetting.GetLeftKey() && !IsOnLeftEdge())
         {
             transform.position += speed * Time.deltaTime * Vector3.left;
         }
-        if (_inputSetting.GetRightKey())
+        if (_inputSetting.GetRightKey() && !IsOnRightEdge())
         {
             transform.position += speed * Time.deltaTime * Vector3.right;
         }
     }
-
     private bool IsOnUpEdge()
     {
-        return true;
+        return cursor.anchoredPosition.y - cursorTipOffsetY > canvas.rect.height;
+    }
+    private bool IsOnDownEdge()
+    {
+        return cursor.anchoredPosition.y - cursorTipOffsetY < 0;
+    }
+    private bool IsOnLeftEdge()
+    {
+        return cursor.anchoredPosition.x - cursorTipOffsetX < 0;
+    }
+    private bool IsOnRightEdge()
+    {
+        return cursor.anchoredPosition.x - cursorTipOffsetX > canvas.rect.width;
     }
 }
