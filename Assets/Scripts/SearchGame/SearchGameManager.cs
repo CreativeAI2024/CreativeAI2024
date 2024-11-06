9bb7ccbeaedfc0e5088c988ac949102d73e7ce45
uwatcha <discoverywant30@gmail.com>
2024-11-06 15:50:18 +0900

    Xキーで終了できるようにした.

--- Assets/Scripts/SearchGame/SearchGameManager.cs
+++ Assets/Scripts/SearchGame/SearchGameManager.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class SearchGameManager : MonoBehaviour
 {
+    private InputSetting _inputSetting;
     [SerializeField] private GameObject main;
     [SerializeField] private SearchGameCursor searchGameCursor;
+    void Start()
+    {
+        _inputSetting = InputSetting.Load();
+    }
 
+    void Update()
+    {
+        if (_inputSetting.GetCancelKeyDown())
+        {
+            Inactivate();
+        }
+    }
     public void Activate()
     {
         main.SetActive(true);
