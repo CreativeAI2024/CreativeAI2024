6ccb3a6cfc1622d4464eb61b7ddf3f913f966e0f
uwatcha <discoverywant30@gmail.com>
2024-11-11 13:55:34 +0900

    wip

--- Assets/Scripts/SearchGame/Effects/BrokenTeddyBearEffect.cs
+++ Assets/Scripts/SearchGame/Effects/BrokenTeddyBearEffect.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class BrokenTeddyBearEffect : MonoBehaviour, IEffectable
 {
+    [SerializeField] private Pause pause;
     public void PlayEffect()
     {
+        pause.PauseAll();
         Debug.Log("テディベアの破れたお腹の中に何かある。");
         Debug.Log("テディベアの心を手に入れた。");
     }
