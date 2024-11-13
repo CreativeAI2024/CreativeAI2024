1e96182aada077370f89e72de37414ae209663a3
KOMATSU CHIKA <128227320+KOMATSU-CHIKA@users.noreply.github.com>
2024-11-13 17:29:37 +0900

    DontStickOut

--- Assets/Scripts/PaperGame/PaperMove.cs
+++ Assets/Scripts/PaperGame/PaperMove.cs
@@ -5,17 +5,18 @@ public class PaperMove : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDra
 {
     private Vector3 _previousPos = Vector3.zero;
     private Vector3 _initialPos;
-    private Transform _transform;
+    private RectTransform _rectTransform;
 
     [SerializeField] private float speed;
-
     private InputSetting _inputSetting;
+    private RectTransform _canvasRectTransform;
 
     private void Start()
     {
-        _transform = this.GetComponent<Transform>();
-
+        _rectTransform = this.GetComponent<RectTransform>();
         _inputSetting = InputSetting.Load();
+
+        _canvasRectTransform = _rectTransform.root.GetComponent<RectTransform>();
     }
 
     private void Update()
@@ -39,25 +40,39 @@ public class PaperMove : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDra
             moveDirection += Vector3.right;
         }
 
-        // 紙オブジェクトをキー入力に基づいて移動
-        _transform.position += moveDirection * speed * Time.deltaTime;
-    }
+        Vector3 newPosition = _rectTransform.localPosition + moveDirection * speed * Time.deltaTime;
 
+        Vector3 clampedPosition = ClampToCanvas(newPosition);
+        _rectTransform.localPosition = clampedPosition;
+    }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         _previousPos = eventData.position;
-        _initialPos = _transform.position;
+        _initialPos = _rectTransform.localPosition;
     }
 
-    public void OnDrag(PointerEventData eventData) 
+    public void OnDrag(PointerEventData eventData)
     {
         Vector3 _currentPos = eventData.position;
         Vector3 diff = _currentPos - _previousPos;
-        _transform.position = _initialPos + new Vector3(diff.x, diff.y);
+        _rectTransform.localPosition = _initialPos + new Vector3(diff.x, diff.y);
+
+        _rectTransform.localPosition = ClampToCanvas(_rectTransform.localPosition);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
+    }
+
+    private Vector3 ClampToCanvas(Vector3 position)
+    {
+        Vector3 minPosition = _canvasRectTransform.rect.min - _rectTransform.rect.min;
+        Vector3 maxPosition = _canvasRectTransform.rect.max - _rectTransform.rect.max;
+
+        float clampedX = Mathf.Clamp(position.x, minPosition.x, maxPosition.x);
+        float clampedY = Mathf.Clamp(position.y, minPosition.y, maxPosition.y);
+
+        return new Vector3(clampedX, clampedY, position.z);
     }
 }
