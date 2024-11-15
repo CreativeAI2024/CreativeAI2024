43b2ada7ae09a93cb56e212e40f15205983c6b29
uwatcha <discoverywant30@gmail.com>
2024-11-15 20:45:05 +0900

    wip

--- Assets/Scripts/SearchGame/Effects/BrokenTeddyBearEffect.cs
+++ Assets/Scripts/SearchGame/Effects/BrokenTeddyBearEffect.cs
@@ -5,6 +5,5 @@ public class BrokenTeddyBearEffect : MonoBehaviour, IEffectable
     public void PlayEffect()
     {
         ConversationTextManager.Instance.InitializeFromString("There are something in the stomach of teddy bear. <br>You got a soul of Teddy Bear.");
-        DebugLogger.Log("Pause. ");
     }
 }
--- Assets/Scripts/SearchGame/Effects/ZombieEffect.cs
+++ Assets/Scripts/SearchGame/Effects/ZombieEffect.cs
@@ -5,6 +5,5 @@ public class ZombieEffect : MonoBehaviour, IEffectable
     public void PlayEffect()
     {
         ConversationTextManager.Instance.InitializeFromString("Henji ga Nai. <br>Tadano Sikabane no Youda.");
-        DebugLogger.Log("Pause. ");
     }
 }
