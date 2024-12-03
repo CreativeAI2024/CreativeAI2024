9a7f8c326e4ce0f63a7cc828a4d3b8eee27de116
uwatcha <discoverywant30@gmail.com>
2024-12-03 20:58:19 +0900

    不要なusingを削除

--- Assets/Scripts/Text/MainTextDrawer.cs
+++ Assets/Scripts/Text/MainTextDrawer.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using System.Diagnostics;
 
 [RequireComponent(typeof(TextMeshProUGUI))]
 public class MainTextDrawer : MonoBehaviour
