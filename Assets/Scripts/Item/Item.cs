01d06dfab0d18a6189367f20f6504a7372bab050
KOMATSU CHIKA <128227320+KOMATSU-CHIKA@users.noreply.github.com>
2025-01-15 02:13:20 +0900

    viewRepotrtText

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 using UnityEngine.Serialization;
 
 [Serializable]
-[CreateAssetMenu(fileName ="Item", menuName ="ScriptableObject/Item/Item")]
+[CreateAssetMenu(fileName = "Item", menuName = "ScriptableObject/Item/Item")]
 public class Item : ScriptableObject
 {
     [SerializeField] private string itemName;
     [SerializeField, TextArea] private string descriptionText;
-    [FormerlySerializedAs("image")] [SerializeField] private Sprite sprite;
+    [FormerlySerializedAs("image")][SerializeField] private Sprite sprite;
     [SerializeField] private string contentTextFilePath;
     public string ItemName => itemName;
     public string DescriptionText => descriptionText;
