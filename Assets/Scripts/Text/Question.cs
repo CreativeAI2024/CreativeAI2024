55d95e0856026a48b014b6d02821d088187ae57b
kanata0510 <54180109+kanata0510@users.noreply.github.com>
2025-01-15 09:07:40 +0900

    Update Assets/Scripts/Text/Question.cs

--- Assets/Scripts/Text/Question.cs
+++ Assets/Scripts/Text/Question.cs
@@ -35,7 +35,7 @@ public class Question : MonoBehaviour
             }
         }
         cursorPlace = 0;
-        if (questionData.All(question => question.Answer != null))
+        if (questionData.Any(question => question.Answer is null))
         {
             float branchCenterPos = 410;
             float branchAreaHeight = questionLen * 75; //75でかけるといい感じになる
