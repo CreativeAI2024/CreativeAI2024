2c25073eaa972ecb551fb8d040155f22ec784dad
kanata0510 <kawakana0510@gmail.com>
2025-02-06 17:18:26 +0900

    fix compile error

--- Assets/Scripts/Text/ChangeBackground.cs
+++ Assets/Scripts/Text/ChangeBackground.cs
@@ -81,31 +81,10 @@ public class ChangeBackground : MonoBehaviour
     {
         foreach(Image image in images)
         {
-            latestChangeImages = changeImages;
-        }
-        if (latestChangeImages != null)
-        {
-            foreach(ChangeImage latestChangeImage in latestChangeImages)
-            {
-                string imageName = latestChangeImage.ImageName;
-                string spriteName = latestChangeImage.SpriteName;
-                if (imageName.Equals("BackgroundPanel")) continue;
-                float brightnessMultiply;
-                if (spriteName.Contains(speakerToSpriteDict[speaker]))
-                {
-                    brightnessMultiply = speakerBrightness;
-                }
-                else
-                {
-                    brightnessMultiply = listenerBrightness;
-                }
-                Color currentColor = imagesDict[imageName].color;
-                float h, s, v;
-                Color.RGBToHSV(currentColor, out h, out s, out v);
-                v = brightnessMultiply;
-                Color newColor = Color.HSVToRGB(h, s, v);
-                imagesDict[imageName].color = newColor;
-            }
+            string imageName = image.name;
+            string spriteName = image.sprite.name;
+            if (imageName.Equals("BackgroundPanel")) continue;
+            ChangeBrightness(imageName, SelectBrightness(speaker, spriteName));
         }
     }
 
@@ -121,4 +100,3 @@ public class ChangeBackground : MonoBehaviour
         imagesDict[imageName].color = newColor;
     }
 }
-
