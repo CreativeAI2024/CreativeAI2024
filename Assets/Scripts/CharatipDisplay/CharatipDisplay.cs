669b8316de620bd0b5cb61f26a2981416945a630
uwatcha <discoverywant30@gmail.com>
2025-02-11 04:10:06 +0900

    Updateで毎フレームフラグの状態を見てしまっているが、Main1後にヒカルのキャラチップを消せた。

--- Assets/Scripts/CharatipDisplay/CharatipDisplay.cs
+++ Assets/Scripts/CharatipDisplay/CharatipDisplay.cs
@@ -3,11 +3,6 @@ using UnityEngine;
 abstract public class CharatipDisplay : MonoBehaviour
 {
     [SerializeField] protected SpriteRenderer charatip;
-    void Start()
-    {
-        DebugLogger.Log($"ChangeCharatipVisibility() added.", DebugLogger.Colors.Yellow);
-        FlagManager.Instance.OnFlagChanged += ChangeCharatipVisibility;
-    }
 
-    protected abstract void ChangeCharatipVisibility();
+    protected abstract void Update();
 }
--- Assets/Scripts/CharatipDisplay/Re-enteredAzamiRoom.cs
+++ Assets/Scripts/CharatipDisplay/Re-enteredAzamiRoom.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Re_enteredAzamiRoom : CharatipDisplay
 {
-    protected override void ChangeCharatipVisibility()
+    protected override void Update()
     {
-    if (FlagManager.Instance.HasFlag("Hikaru_Back_To_Mirror_Room"))
+    if (FlagManager.Instance.HasFlag("Hikaru_Back_To_Mirror_Room")/* && SceneManager.GetActiveScene().name.Equals(belongSceneName)*/)
         {
             DebugLogger.Log($"HikaruMeetToAzami Hidden.", DebugLogger.Colors.Yellow);
             charatip.enabled = false;
-            FlagManager.Instance.DeleteFlag("Hikaru_Back_To_Mirror_Room");
         }
     }
+    // Actionに関数をAddしても、破棄されたシーン内の関数は実行しようとしてもSpriteRendererなどが失われてしまっていた。
+    // DontDestroyのCharatipDisplayManagerを作り、そこにGameObjectごとListにAddしても、シーンが変わるとアクセスできなかった。
+    // 仕方がないので、Updateで行う
 }
