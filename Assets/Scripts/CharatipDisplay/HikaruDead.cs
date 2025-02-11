22a0dd60ac50cbdf8a0d28d40d67adfcfba5f909
uwatcha <discoverywant30@gmail.com>
2025-02-11 17:00:05 +0900

    ヒカルの死体と血溜まりのキャラチップを追加した

--- Assets/Scripts/CharatipDisplay/HikaruDead.cs
+++ Assets/Scripts/CharatipDisplay/HikaruDead.cs
@@ -3,10 +3,15 @@ public class HikaruDead : CharatipDisplay
     public override void ChangeCharatipVisibility()
     {
         // 普段の立ち絵、死体、血溜まりのSpriteを持っておき、条件に合わせて複数のSpriteRendererを操作する。
-        if (!charatip.enabled && FlagManager.Instance.HasFlag("HikaruDead"))
+        if (!charatip.enabled && FlagManager.Instance.HasFlag("Hikaru_Dead") && !FlagManager.Instance.HasFlag("Hikaru_Eaten"))
         {
             DebugLogger.Log($"HikaruDead Displayed.", DebugLogger.Colors.Yellow);
             charatip.enabled = true;
+        }
+        if (charatip.enabled && FlagManager.Instance.HasFlag("Hikaru_Eaten"))
+        {
+            DebugLogger.Log($"HikaruDead Hidden.", DebugLogger.Colors.Yellow);
+            charatip.enabled = false;
         }
     }
 }
