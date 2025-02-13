4181d92c46cd77952917868526f04fbe766894bf
uwatcha <discoverywant30@gmail.com>
2025-02-14 00:05:21 +0900

    Tessen_Talkが発生しない問題を修正. Progress2がfalseに戻されていない特殊仕様を修正し、今までこれを参照していた部分も修正. AzamiRush内でアザミを鏡の前に呼ぶ処理を消した。AzamiRushの位置を微調整

--- Assets/Scripts/CharatipDisplay/AzamiRush.cs
+++ Assets/Scripts/CharatipDisplay/AzamiRush.cs
@@ -14,13 +14,5 @@ public class AzamiRush : CharatipDisplay
             DebugLogger.Log($"AzamiRush Hidden.", DebugLogger.Colors.Yellow);
             charatip.enabled = false;
         }
-
-        if (!charatip.enabled && FlagManager.Instance.HasFlag("Progress10"))
-        {
-            DebugLogger.Log($"AzamiRush Displayed.", DebugLogger.Colors.Yellow);
-            charatip.enabled = true;
-            Vector3 newPosition = new Vector3(5,1.5f,0);
-            charatip.transform.position = newPosition;
-        }
     }
 }
